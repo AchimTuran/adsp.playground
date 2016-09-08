f95a150b003069a61b2ee351b25297a59006dacd
AchimTuran <mail@achim-turan.de>
2016-09-08 20:26:45 +0200

    [CompressorMode] Request model state during mode creation

--- src/CompressorMode/CompressorMode.cpp
+++ src/CompressorMode/CompressorMode.cpp
@@ -115,6 +115,18 @@ AE_DSP_ERROR CCompressorMode::ModeCreate(const AE_DSP_SETTINGS &Settings, const 
     return AE_DSP_ERROR_FAILED;
   }
 
+  if (CAddonProcessManager::ConnectObject(this) != 0)
+  {
+    KODI->Log(LOG_ERROR, "%s, %i, Failed to connect message dispachter %s", __FUNCTION__, __LINE__, this->Name.c_str());
+    return AE_DSP_ERROR_FAILED;
+  }
+
+  if (!this->SendMsg(nullptr, 0, CSocketCompressorModeIDs::RequestModelState))
+  {
+    KODI->Log(LOG_ERROR, "%s, %i, Failed send \"%s\" from dispatcher %s to compressor controller", __FUNCTION__, __LINE__, CSocketCompressorModeIDs::ToString(CSocketCompressorModeIDs::RequestModelState), this->Name.c_str());
+    return AE_DSP_ERROR_FAILED;
+  }
+
   return AE_DSP_ERROR_NO_ERROR;
 }
 
